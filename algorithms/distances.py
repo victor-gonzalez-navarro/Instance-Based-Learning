b993f7eb17feb89f1ad734fefc50a6001db6ea61
Olga Valls <ovalls@gmail.com>
2018-12-01 22:39:10 +0100

    distances

--- algorithms/distances.py
+++ algorithms/distances.py
@@ -27,3 +27,35 @@ def manhattan(a, b):
             if ai != bi:
                 distance = distance + 1
     return distance
+
+
+def cosine(a, b):
+    distance_nom = 0
+    distance_den1 = 0
+    distance_den1 = 0
+    for ai, bi in zip(a,b):
+        if type(ai) in [float, np.float64]:
+            distance_nom = distance_nom + (ai*bi)
+            distance_den1 = distance_den1 + ai**2
+            distance_den2 = distance_den2 + bi**2
+        else:
+            if ai != bi:
+                distance = distance + 1
+    distance = distance_nom / (distance_den1*distance_den2)
+    return distance
+
+
+def canberra(a, b):
+    distance = 0
+    for ai, bi in zip(a,b):
+        if type(ai) in [float, np.float64]:
+            distance = distance + abs(ai-bi)/(abs(ai)+abs(bi))
+        else:
+            if ai != bi:
+                distance = distance + 1
+    return distance
+
+
+# Clark ¿?
+
+# HVDM ¿?
